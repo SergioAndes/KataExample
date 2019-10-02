3f321e04bdec21c87f1dd221ec8848da073a6ce9
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-10-01 23:33:00 -0500

    Iteracion 3.4
    Iteracion 3, n numeros verde

--- untitled2/KataSimple.py
+++ untitled2/KataSimple.py
@@ -20,17 +20,23 @@ def iteracion2(cadena):
         for num in numeros:
             cantidad += 1
 
-        return [cantidad,int(min(numeros))]
+        return [cantidad, int(min(numeros))]
     else:
-        return [1,int(min(cadena))]
+        return [1, int(min(cadena))]
+
 
 def iteracion3(cadena):
     if cadena == "":
         return [0, 0, 0]
     elif "," in cadena:
-        return[2,1,2]
+        numeros = cadena.split(",")
+        cantidad = 0
+        for num in numeros:
+            cantidad += 1
+
+        return [cantidad, int(min(numeros)),int(max(numeros))]
     else:
-        return [1,1,1]
+        return [1, int(min(cadena)),int(max(cadena))]
 
 
 class KataSimple:
