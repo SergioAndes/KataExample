61eb04ad8f772a25d4994654a4fc28441a24affc
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-10-01 22:41:39 -0500

    iteracion1.3
    Se agrega prueba para probar con dos numero (Rojo)

--- untitled2/KataSimple.py
+++ untitled2/KataSimple.py
@@ -1,8 +1,8 @@
 def iteracion1(cadena):
     if cadena== "":
-        return 0
+        return [0]
     else:
-        return 1
+        return [1]
 
 
 
--- untitled2/KataSimpleTest.py
+++ untitled2/KataSimpleTest.py
@@ -5,7 +5,11 @@ from KataSimple import iteracion1
 
 class kataSimpleTest(TestCase):
     def test_iteracion1(self):
-        self.assertEqual(iteracion1(""), 0, "Cadena vacia")
+        self.assertEqual(iteracion1(""), [0], "Cadena vacia")
 
-    def test_iteracion1_unaCadena(self):
-        self.assertEqual(iteracion1("1"), 1, "Un numero")
+    def test_iteracion1_unNuermo(self):
+        self.assertEqual(iteracion1("1"), [1], "Un numero")
+
+    def test_iteracion1_dosNumeros(self):
+        self.assertEqual(iteracion1("1,2"), [2], "Dos numeros")
+
