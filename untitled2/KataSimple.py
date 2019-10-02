2101de5a927ad25fef8c7b8e322c7d51f49329b2
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-10-01 23:50:20 -0500

    iteracion 4.4
    Iteracion 4, n numeros (Verde)

--- untitled2/KataSimple.py
+++ untitled2/KataSimple.py
@@ -43,9 +43,16 @@ def iteracion4(cadena):
     if cadena == "":
         return [0, 0, 0,0]
     elif "," in cadena:
-        return [2, 1, 2, 1.5]
+        numeros = cadena.split(",")
+        cantidad = 0
+        suma = 0
+        for num in numeros:
+            cantidad += 1
+            suma = int(num)+suma
+
+        return [cantidad, int(min(numeros)), int(max(numeros)),suma/cantidad]
     else:
-        return [1, 1, 1, 1]
+        return [1, int(min(cadena)), int(max(cadena)), 1]
 
 
 class KataSimple:
