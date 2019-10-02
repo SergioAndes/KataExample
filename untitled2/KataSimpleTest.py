1bc309e4a16a8b69ba22d9d44b4178140902507b
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-10-01 22:33:59 -0500

    iteracion1.2
    Se agrega prueba para probar con un numero (Rojo)

--- untitled2/KataSimpleTest.py
+++ untitled2/KataSimpleTest.py
@@ -6,3 +6,6 @@ from KataSimple import iteracion1
 class kataSimpleTest(TestCase):
     def test_iteracion1(self):
         self.assertEqual(iteracion1(""), 0, "Cadena vacia")
+
+    def test_iteracion1_unaCadena(self):
+        self.assertEqual(iteracion1("1"), 1, "Un numero")
